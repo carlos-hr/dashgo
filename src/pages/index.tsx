d0e9e3e84313fbfb52548c2475c00d4453051666
Carlos-hr <carlos.henrique17.chr@gmail.com>
2022-06-01 20:22:44 -0300

    feat(signIn): creating mock design of signin page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,53 @@
+import { Button, Flex, FormLabel, Input, Stack } from "@chakra-ui/react";
+
 export default function Home() {
   return (
-    <div>
-      <h1>DashGo</h1>
-    </div>
+    <Flex w="100vw" h="100vh" align="center" justify="center">
+      <Flex
+        as="form"
+        w="100%"
+        maxW="360"
+        bg="gray.800"
+        p="8"
+        borderRadius="8"
+        flexDir="column"
+      >
+        <Stack spacing="4">
+          <>
+            <FormLabel htmlFor="email">E-mail</FormLabel>
+            <Input
+              id="email"
+              name="email"
+              type="email"
+              focusBorderColor="pink.500"
+              bgColor="gray.900"
+              variant="filled"
+              _hover={{
+                bgColor: "gray.900",
+              }}
+              size="lg"
+            />
+          </>
+          <>
+            <FormLabel htmlFor="password">Senha</FormLabel>
+            <Input
+              id="password"
+              name="password"
+              type="password"
+              focusBorderColor="pink.500"
+              bgColor="gray.900"
+              variant="filled"
+              _hover={{
+                bgColor: "gray.900",
+              }}
+              size="lg"
+            />
+          </>
+        </Stack>
+        <Button type="submit" mt="6" colorScheme="pink" size="lg">
+          Entrar
+        </Button>
+      </Flex>
+    </Flex>
   );
 }
