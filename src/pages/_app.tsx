e44d9719ccd2d82c3878ca917151b453d6cf05c4
Carlos-hr <carlos.henrique17.chr@gmail.com>
2022-06-04 21:09:00 -0300

    feat(mirage): setting up and creating a database mock with mirage

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,10 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import { AppProps } from "next/app";
 import { SidebarProvider } from "../contexts/SideBarContext";
+import { makeServer } from "../services/mirage";
 import { theme } from "../styles/theme";
+
+if (process.env.NODE_ENV === "development") makeServer();
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
